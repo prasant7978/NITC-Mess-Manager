e207eae0f2569da9386bbba8980ddebf68bb4a21
Prasanta Ku Sethi <sethip52@gmail.com>
2023-03-07 15:54:53 +0530

    Added Sign-out for student.

--- app/src/main/java/com/nitc/nitcmessmanager/StudentDashboardActivity.kt
+++ app/src/main/java/com/nitc/nitcmessmanager/StudentDashboardActivity.kt
@@ -5,13 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.R
+//import com.google.firebase.database.R
 import com.nitc.nitcmessmanager.databinding.ActivityStudentDashboardBinding
 
 class StudentDashboardActivity : AppCompatActivity() {
 
-    lateinit var dashboardBinding: ActivityStudentDashboardBinding
+    private lateinit var dashboardBinding: ActivityStudentDashboardBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,5 +23,21 @@ class StudentDashboardActivity : AppCompatActivity() {
         setContentView(view)
 
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_signout,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.signOut){
+            FirebaseAuth.getInstance().signOut()
+            Toast.makeText(this,"Sign out is successfull", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this@StudentDashboardActivity,LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        return true
     }
 }
