e61af33cef993e5d3525ece557600b72f2d7d14c
Prasanta Ku Sethi <sethip52@gmail.com>
2023-03-07 15:27:47 +0530

    Added new menu design.

--- app/src/main/java/com/nitc/nitcmessmanager/StudentDashboardActivity.kt
+++ app/src/main/java/com/nitc/nitcmessmanager/StudentDashboardActivity.kt
@@ -1,11 +1,26 @@
 package com.nitc.nitcmessmanager
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.R
+import com.nitc.nitcmessmanager.databinding.ActivityStudentDashboardBinding
 
 class StudentDashboardActivity : AppCompatActivity() {
+
+    lateinit var dashboardBinding: ActivityStudentDashboardBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_student_dashboard)
+
+        dashboardBinding = ActivityStudentDashboardBinding.inflate(layoutInflater)
+        val view = dashboardBinding.root
+
+        setContentView(view)
+
+
     }
 }
