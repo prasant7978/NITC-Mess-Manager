f8a6c7bcbe8c1850653d66874c09c93e028b2c05
Gaurav <gauravgupta1999a@gmail.com>
2023-03-08 22:17:12 +0530

    Updated Contractor Dashboard Design and generate bill design

---
+++ app/src/main/java/com/nitc/nitcmessmanager/GenerateBill.kt
@@ -0,0 +1,11 @@
+package com.nitc.nitcmessmanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GenerateBill : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_generate_bill)
+    }
+}
